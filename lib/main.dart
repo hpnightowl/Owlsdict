f014e1ac0161f91e393a05035c3e7b1b50d0cee7
hpnightowl <iamhp2k@gmail.com>
2021-06-25 22:49:06 +0530

    owlsdict: init working: fetch word from api and search word

--- lib/main.dart
+++ lib/main.dart
@@ -1,58 +1,156 @@
-import 'package:flutter/material.dart';
+import 'dart:async';
+import 'dart:convert';
 
-void main() {
-  runApp(MyApp());
-}
+import 'package:flutter/material.dart';
+import 'package:http/http.dart';
+
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'OwlDict',
-         home: MyHomePage(title: 'Owls Dictionary'),
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
-
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  String _url = "https://owlbot.info/api/v4/dictionary/";
+  String _token = "";
+
   TextEditingController _controller = TextEditingController();
+
+  late StreamController _streamController;
+  late Stream _stream;
+
+  late Timer _debounce;
+
+  _search() async {
+    if (_controller.text == null || _controller.text.length == 0) {
+      _streamController.add(null);
+      return;
+    }
+
+    _streamController.add("waiting");
+    Response response = await get(Uri.parse(_url + _controller.text.trim()), headers: {"Authorization": "Token " + _token});
+    _streamController.add(json.decode(response.body));
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    _streamController = StreamController();
+    _stream = _streamController.stream;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-          title: Text(widget.title),
-      ),
       bottomSheet: BottomAppBar(
-        child: Row(
-          children: [
-            Expanded(
+        child: PreferredSize(
+          preferredSize: Size.fromHeight(48.0),
+          child: Row(
+            children: <Widget>[
+              Expanded(
                 child: Container(
-                  color: Colors.blue,
+                  margin: const EdgeInsets.only(left: 12.0, bottom: 8.0),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(24.0),
+                  ),
                   child: TextFormField(
-                    onChanged: (String text){
+                    onChanged: (String text) {
+                      if (_debounce?.isActive ?? false) _debounce.cancel();
+                      _debounce = Timer(const Duration(milliseconds: 1000), () {
+                        _search();
+                      });
                     },
                     controller: _controller,
                     decoration: InputDecoration(
-                      hintText: "Search For word",
+                      hintText: "Search for a word",
+                      hintStyle: TextStyle(fontWeight: FontWeight.w700, color: Colors.blue),
+                      contentPadding: const EdgeInsets.only(left: 24.0),
+                      enabledBorder: new OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.blue),
+                        borderRadius: BorderRadius.circular(20)
+                      ),
                     ),
+                  ),
+                ),
               ),
-                )
-            ),
-          ],
+              IconButton(
+                icon: Icon(
+                  Icons.search,
+                  color: Colors.black,
+                ),
+                onPressed: () {
+                  _search();
+                },
+              )
+            ],
+          ),
         ),
       ),
       body: Container(
-      )
+        margin: const EdgeInsets.all(8.0),
+        child: StreamBuilder(
+          stream: _stream,
+          builder: (BuildContext ctx, AsyncSnapshot snapshot) {
+            if (snapshot.data == null) {
+              return Center(
+                child: Row(
+                  children: [
+                    Image(image: image)
+                  ],
+                )
+              );
+            }
+
+            if (snapshot.data == "waiting") {
+              return Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+
+            return ListView.builder(
+              itemCount: snapshot.data["definitions"].length,
+              itemBuilder: (BuildContext context, int index) {
+                return ListBody(
+                  children: <Widget>[
+                    Container(
+                      color: Colors.grey[300],
+                      child: ListTile(
+                        leading: snapshot.data["definitions"][index]["image_url"] == null
+                            ? null
+                            : CircleAvatar(
+                          backgroundImage: NetworkImage(snapshot.data["definitions"][index]["image_url"]),
+                        ),
+                        title: Text(_controller.text.trim() + "(" + snapshot.data["definitions"][index]["type"] + ")"),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text(snapshot.data["definitions"][index]["definition"]),
+                    )
+                  ],
+                );
+              },
+            );
+          },
+        ),
+      ),
     );
   }
 }
